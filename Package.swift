a865960759cfa32cb65ef9fa1ae2fc9ff9895f41
skipp <chanchanakoedtho@gmail.com>
2022-06-05 22:55:08 +0700

    add new package version

--- Package.swift
+++ Package.swift
@@ -22,12 +22,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "UnityFramework",
-                      url: "https://github.com/sk-chanch/RunYourWay-Unity-Framework/releases/download/1.1.9/UnityFramework.zip",
-                      checksum:"2022-06-05 22:30:50.983 xcodebuild[9099:153590] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionSentinelHostApplications for extension Xcode.DebuggerFoundation.AppExtensionHosts.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
-2022-06-05 22:30:50.984 xcodebuild[9099:153590] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionPointIdentifierToBundleIdentifier for extension Xcode.DebuggerFoundation.AppExtensionToBundleIdentifierMap.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
-2022-06-05 22:30:51.960 xcodebuild[9100:153623] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionSentinelHostApplications for extension Xcode.DebuggerFoundation.AppExtensionHosts.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
-2022-06-05 22:30:51.960 xcodebuild[9100:153623] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionPointIdentifierToBundleIdentifier for extension Xcode.DebuggerFoundation.AppExtensionToBundleIdentifierMap.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
-e92dc6cc39ad2b87ee4cbc9a1e273667dac3f314d2933e4c0d12e356193e43b7"),
+                      url: "https://github.com/sk-chanch/RunYourWay-Unity-Framework/releases/download/1.2.0/UnityFramework.zip",
+                      checksum:"e92dc6cc39ad2b87ee4cbc9a1e273667dac3f314d2933e4c0d12e356193e43b7"),
         
 //            .target(
 //                name: "UnityFrameworkWrapper",
